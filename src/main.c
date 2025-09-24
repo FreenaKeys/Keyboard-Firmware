aefdd515436cc0a542fd6e4cbf7e5376bda34073
CloverTech1105 <76156149+yukkuri-Dev@users.noreply.github.com>
2025-09-24 16:12:35 +0900

    First Commit(No)

--- src/main.c
+++ src/main.c
@@ -1,1 +1,8 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+void app_main() {
+    printf("Hello world!\n"); 
+}
